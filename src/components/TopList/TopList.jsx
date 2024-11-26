5ae077ad41b07d49432e8c2096ff37ef79291b6a
josecalleja <jose@tslatam.com>
2024-11-26 16:52:46 -0300

    permisossss

--- src/components/TopList/TopList.jsx
+++ src/components/TopList/TopList.jsx
@@ -5,24 +5,31 @@ const TopList = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Función para obtener los datos de la API
     const fetchProducts = async () => {
       try {
-        const response = await fetch("https://djangoventi.onrender.com/api/products/");
+        const response = await fetch("https://djangoventi.onrender.com/api/products/", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer <78cbe3644fa4d10b87e6f07be60fce2445a92ff6>", // Reemplaza <TU_TOKEN> con el token generado
+          },
+        });
+
         if (!response.ok) {
-          throw new Error("Error al obtener los datos");
+          throw new Error(`Error HTTP: ${response.status}`);
         }
+
         const data = await response.json();
-        setProducts(data); // Asigna los datos obtenidos al estado
+        setProducts(data);
       } catch (error) {
         console.error("Error al cargar los productos:", error);
       } finally {
-        setLoading(false); // Deja de mostrar el indicador de carga
+        setLoading(false);
       }
     };
 
     fetchProducts();
-  }, []); // Se ejecuta solo una vez al montar el componente
+  }, []);
 
   if (loading) {
     return <p>Cargando productos...</p>;
@@ -30,19 +37,17 @@ const TopList = () => {
 
   return (
     <div className="container py-14">
-      {/* header section */}
       <div className="text-center mb-12">
         <h1 className="text-4xl font-semibold">Nuestras pizzas</h1>
         <p>Este es nuestro top!</p>
       </div>
-      {/* card section */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
         {products.map((item, index) => (
           <div
             key={index}
             className="bg-white/30 p-5 sm:p-7 lg:p-6 rounded-3xl hover:scale-110 transition duration-300"
           >
-           
+          
             <div className="space-y-2">
               <p className="text-red-500">{item.rating || "⭐⭐⭐⭐⭐"}</p>
               <p className="text-lg font-semibold">{item.name}</p>
